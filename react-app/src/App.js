75d25c5ff13e47661e6adbcf8ae075879a8c972e
Samuel Hovie <samhovie@umich.edu>
2023-05-16 19:14:20 -0400

    splash page image

---
+++ react-app/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import SignupFormPage from "./components/SignupFormPage";
+import LoginFormPage from "./components/LoginFormPage";
+import { authenticate } from "./store/session";
+import Navigation from "./components/Navigation";
+import { useSelector } from "react-redux";
+import SplashPage from "./components/SplashPage";
+
+function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const sessionUser = useSelector((state) => state.session.user)
+
+  useEffect(() => {
+    dispatch(authenticate()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return (
+    <>
+      {sessionUser && <Navigation isLoaded={isLoaded} />}
+      {isLoaded && (
+        <Switch>
+          <Route path="/" >
+            <SplashPage />
+          </Route>
+          <Route path="/login" >
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/SplashPage/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './SplashPage.css'
+
+export default function SplashPage() {
+    return (
+       <div className="splash-page">SplashPage</div>
+    )
+}
