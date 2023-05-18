ad965d6b282cc5af86a2fbf025ac902d9963a78b
Samuel Hovie <samhovie@umich.edu>
2023-05-18 18:58:45 -0400

    nav

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+    const sessionUser = useSelector((state) => state.session.user);
 
-	return (
-		<ul className="navigation flex ">
-			<li>
-				<NavLink exact to="/">SoundForum</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
-	);
+    return (
+        <div className="navigation-container flex justify-center">
+            <ul className="navigation flex align-center space-between">
+                <li>
+                    <NavLink exact to="/">
+                        <div className="flex align-center">
+                            <img
+                                className="icon"
+                                src="https://livebnbbucket.s3.amazonaws.com/favicon.svg"
+                            ></img>
+                            <h1 className="main-title">SoundForum</h1>
+                        </div>
+                    </NavLink>
+                </li>
+                {isLoaded && (
+                    <li>
+                        <ProfileButton user={sessionUser} />
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default Navigation;
