720d887bed1fd0ef7709a73dd4012c9449acf2a1
Samuel Hovie <samhovie@umich.edu>
2023-05-19 00:42:05 -0400

    delete progress

--- react-app/src/components/FeedPage/index.js
+++ react-app/src/components/FeedPage/index.js
@@ -1,20 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import { getAllQuestionsThunk } from '../../store/question';
-import './FeedPage.css'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllQuestionsThunk } from "../../store/question";
+import "./FeedPage.css";
+import { useModal } from "../../context/Modal";
+import OpenModalButton from "../OpenModalButton";
 
 export default function FeedPage() {
-
-    const dispatch = useDispatch()
-    const questions = Object.values(useSelector((state) => state.questions.allQuestions))
+    const dispatch = useDispatch();
+    const questions = Object.values(
+        useSelector((state) => state.questions.allQuestions)
+    );
+    const { closeModal } = useModal()
 
     useEffect(() => {
-        dispatch(getAllQuestionsThunk())
-    }, [dispatch])
+        dispatch(getAllQuestionsThunk());
+    }, [dispatch]);
+
+    function handleDeleteQuestion(id) {
+        console.log('hello', id)
+        // dispatch(deleteQuestionThunk(id))
+        closeModal()
+    }
+
+
 
     return (
-            <>
-            {questions.map(question => (<p key={question.id}>{question.text}</p>))}
-            </>
-    )
+        <div className="feed">
+            {questions.map((question) => (
+                <div key={question.id}>
+                    {question.text}
+                    {/* <button>Delete</button> */}
+                    <OpenModalButton
+                        className='delete-question'
+                        modalComponent={<div><button onClick={() => handleDeleteQuestion(question.id)}>DELETE?</button></div>}
+                        buttonText="Delete"
+                    />
+                    <button>Update</button>
+                    </div>
+            ))}
+        </div>
+    );
 }
