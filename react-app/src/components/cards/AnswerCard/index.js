a1959bcd06b7c7b4cc1d12571bf8323a105bc8b3
Samuel Hovie <samhovie@umich.edu>
2023-05-24 21:08:53 -0400

    some cleanup

--- react-app/src/components/cards/AnswerCard/index.js
+++ react-app/src/components/cards/AnswerCard/index.js
@@ -1,26 +1,13 @@
 import React from "react";
 import "./AnswerCard.css";
-import "../Card.css"
+import "../Card.css";
 import CardHeader from "../CardContainer/CardHeader";
 import OpenModalButton from "../../modals/OpenModalButton";
-import { deleteAnswerThunk } from "../../../store/answer";
-import { getQuestionThunk } from "../../../store/question";
-import { useDispatch, useSelector } from "react-redux";
-import { useModal } from "../../../context/Modal";
 import UpdateAnswerModal from "../../modals/UpdateAnswerModal";
 import DeleteAnswerModal from "../../modals/DeleteAnswerModal";
 
 export default function AnswerCard({ answer }) {
     const sessionUser = useSelector((state) => state.session.user);
-    const { closeModal } = useModal();
-    const dispatch = useDispatch();
-
-    async function handleDeleteAnswer(answer) {
-        const id = answer.question_id;
-        await dispatch(deleteAnswerThunk(answer.id));
-        await dispatch(getQuestionThunk(id));
-        closeModal();
-    }
 
     return (
         <>
@@ -33,19 +20,7 @@ export default function AnswerCard({ answer }) {
                 <div className="card card-button-row">
                     <OpenModalButton
                         className="delete-question"
-                        modalComponent={
-                            <DeleteAnswerModal answer={answer}/>
-                            // <div>
-                            //     <button
-                            //         onClick={(e) => {
-                            //             e.preventDefault();
-                            //             handleDeleteAnswer(answer);
-                            //         }}
-                            //     >
-                            //         DELETE?
-                            //     </button>
-                            // </div>
-                        }
+                        modalComponent={<DeleteAnswerModal answer={answer} />}
                         buttonText="Delete"
                     />
                     <OpenModalButton
--- react-app/src/components/pages/QuestionDetailPage/index.js
+++ react-app/src/components/pages/QuestionDetailPage/index.js
@@ -5,9 +5,6 @@ import { useParams } from "react-router-dom";
 import QuestionCard from "../../cards/QuestionCard";
 import { getQuestionThunk } from "../../../store/question";
 import { useModal } from "../../../context/Modal";
-import { updateAnswerThunk } from "../../../store/answer";
-import { deleteAnswerThunk } from "../../../store/answer";
-import OpenModalButton from "../../modals/OpenModalButton";
 import CardContainer from "../../cards/CardContainer";
 import AnswerCard from "../../cards/AnswerCard";
 
@@ -28,13 +25,6 @@ export default function QuestionDetail() {
     if (!question.answers) {
         return null;
     }
-
-    async function handleDeleteAnswer(answer) {
-        await dispatch(deleteAnswerThunk(answer.id));
-        await dispatch(getQuestionThunk(answer.question_id));
-        closeModal();
-    }
-
     return (
         <div className="page">
             <CardContainer
