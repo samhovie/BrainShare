3e7ab9e039293fc5602b605d93ae56cf86e9e65e
Samuel Hovie <samhovie@umich.edu>
2023-10-13 20:14:54 -0400

    cleanup

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -25,7 +25,6 @@ export default function Navigation({ isLoaded }) {
                 </li>
 
                 <li className="flex">
-                    {/* <input className="search" placeholder="Search"></input> */}
                     <ProfileButton user={sessionUser} />
                 </li>
 
